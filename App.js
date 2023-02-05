c27d675d3971c41edf3ea46a4c318a10076bbe4d
vimalds15 <antovimalands@gmail.com>
2023-02-05 21:01:34 +0530

    implemented bottom tab navigation

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+
+import HomeScreen from "./src/app/screens/HomeScreen";
+import BookingScreen from "./src/app/screens/BookingScreen";
+import SupportScreen from "./src/app/screens/SupportScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="home"
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarActiveTintColor: "black",
+          tabBarInactiveTintColor: "gray",
+          tabBarIconStyle: {
+            minHeight: 40,
+          },
+        }}
+      >
+        <Tab.Screen
+          name="home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <MaterialIcons name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="booking"
+          component={BookingScreen}
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <MaterialIcons name="list-alt" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="support"
+          component={SupportScreen}
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <MaterialIcons name="support-agent" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
